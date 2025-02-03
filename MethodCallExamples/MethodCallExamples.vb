a3b64ade2314729ec0e8111d12933a94078144aa
Noah Holloway <noahholloway23@gmail.com>
2025-02-03 08:57:27 -0700

    added another sub and insterted it into main sub

---
+++ MethodCallExamples/MethodCallExamples.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
+Module MethodCallExamples
+
+    Sub Main()
+        Dim myLuckyNumber As Integer = 7
+        Console.WriteLine("Hello from Sub Main")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        MyOtherSub() 'calls upon my other sub, like making a very large variable
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+
+    End Sub
+
+    Sub MyOtherSub()
+        Dim myLuckyNumber As Integer = 5
+        Console.WriteLine("Hello from MyOtherSub!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+
+    End Sub
+End Module
