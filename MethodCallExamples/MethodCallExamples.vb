c67059a25754e6e255741332aba432de677d4cb8
Noah Holloway <noahholloway23@gmail.com>
2025-02-04 08:26:04 -0700

    add another sub, sandwich

--- MethodCallExamples/MethodCallExamples.vb
+++ MethodCallExamples/MethodCallExamples.vb
@@ -7,15 +7,23 @@ Module MethodCallExamples
         Dim myLuckyNumber As Integer = 7
         Console.WriteLine("Hello from Sub Main")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
-        MyOtherSub() 'calls upon my other sub, like making a very large variable
+        Console.WriteLine()
+        ' MyOtherSub() 'calls upon my other sub, like making a very large variable
+        Sandwich(myLuckyNumber)
+        Console.WriteLine("Hello from Sub Main")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
-
+        Console.WriteLine()
     End Sub
 
     Sub MyOtherSub()
         Dim myLuckyNumber As Integer = 5
         Console.WriteLine("Hello from MyOtherSub!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
+    End Sub
 
+    Sub Sandwich(ByVal myLuckyNumber As Integer)
+        Console.WriteLine("Hello from Sandwich!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
     End Sub
 End Module
