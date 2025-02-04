43df4d529718b067bc746e4e9d1c36b7d600085c
Noah Holloway <noahholloway23@gmail.com>
2025-02-04 08:42:33 -0700

    demonstrate byRef arguments and the extension of the variable scope

--- MethodCallExamples/MethodCallExamples.vb
+++ MethodCallExamples/MethodCallExamples.vb
@@ -5,13 +5,14 @@ Module MethodCallExamples
 
     Sub Main()
         Dim myLuckyNumber As Integer = 7
-        Console.WriteLine("Hello from Sub Main")
+        Console.WriteLine("Hello from Sub Main!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
         Console.WriteLine()
 
-        ' MyOtherSub() 'calls upon my other sub, like making a very large variable
+        MyOtherSub() 'calls upon my other sub, like making a very large variable
         Sandwich(myLuckyNumber)
-        Console.WriteLine("Hello from Sub Main")
+        Marine(myLuckyNumber)
+        Console.WriteLine("Hello from Sub Main!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
         Console.WriteLine()
     End Sub
@@ -20,11 +21,19 @@ Module MethodCallExamples
         Dim myLuckyNumber As Integer = 5
         Console.WriteLine("Hello from MyOtherSub!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
     End Sub
 
     Sub Sandwich(ByVal myLuckyNumber As Integer)
         myLuckyNumber += 5
         Console.WriteLine("Hello from Sandwich!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
+    End Sub
+
+    Sub Marine(ByRef myLuckyNumber As Integer)
+        myLuckyNumber += 7
+        Console.WriteLine("Hello from Sub Marine!!")
         Console.WriteLine($"My lucky number is {myLuckyNumber}")
         Console.WriteLine()
     End Sub
